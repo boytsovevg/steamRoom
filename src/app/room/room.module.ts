c6268b56970f2bed88382c0dacf9e4de9b439c8f
Eugene <evgeniy.boytsov@gmail.com>
2018-06-13 13:14:45 +0300

    add ng-bootstrap, add room module, add room-component

---
+++ src/app/room/room.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RoomComponent } from './room/room.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RoomComponent]
+})
+export class RoomModule { }
