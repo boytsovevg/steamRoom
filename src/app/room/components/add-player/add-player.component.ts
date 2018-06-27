499b7e993806e3626942d975c2fc7e196e1dbe8a
Evgeniy Boytsov <evgeniy.boytsov@gmail.com>
2018-06-27 15:42:54 +0300

    add player$ subscribtion

--- src/app/room/components/add-player/add-player.component.ts
+++ src/app/room/components/add-player/add-player.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { Player, PlayersDataService } from '../../../core';
 
 @Component({
@@ -8,30 +10,31 @@ import { Player, PlayersDataService } from '../../../core';
 })
 export class AddPlayerComponent {
 
-    public player: Player;
+    public player$: Observable<Player>;
     public noResults = false;
 
+    private player: Player;
     @Output() onPlayerAdd = new EventEmitter<Player>();
 
     constructor(private playersService: PlayersDataService) { }
 
     public getPlayerByName(event: any): void {
 
-        this.playersService.getPlayerByName(event.target.value)
-            .subscribe((player: Player) => {
-                if (!player) {
-                    this.noResults = true;
-                    this.player = null;
-                    return;
-                }
+        this.player$ = this.playersService.getPlayerByName(event.target.value);
 
-                this.noResults = false;
-                this.player = player;
-            });
+        this.player$.subscribe(player => {
+            if (!player) {
+                this.noResults = true;
+                return null;
+            }
+
+            this.noResults = false;
+            this.player = player;
+        });
     }
 
     public addPlayer(): void {
         this.onPlayerAdd.emit(this.player);
-        this.player = null;
+        this.player$ = null;
     }
 }
